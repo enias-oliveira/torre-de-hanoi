b73455eaf8e016c9220a3b8f9c06f1bbf1f04dab
enias-c137 <eniasoliveira@gmail.com>
2020-09-14 16:09:13 -0300

    Funções iniciais adicionadas. Agora é possível mover um disco de uma torre para outra.

---
+++ script.js
@@ -0,0 +1,24 @@
+let selectorMode = true
+let selectedTorre = undefined
+
+document.querySelectorAll(".torres").forEach(item => item.addEventListener('click',moveDisco))
+
+function moveDisco(e) {
+    
+    let discInTorre = e.currentTarget.childElementCount
+
+    if (selectorMode) {
+        if (discInTorre) {
+            selectedTorre = e.currentTarget
+            selectorMode = false
+            console.log(selectedTorre)
+        }
+        
+    } else (insertDisco(e.currentTarget))
+}
+
+
+function insertDisco(e) {
+    e.appendChild(selectedTorre.lastElementChild)
+    selectorMode = true
+}
