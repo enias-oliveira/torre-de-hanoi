ce9c56444baeb39e5cc9673d030d5f7335a87ce5
Alexandre Alves <alexandrealvescs@gmail.com>
2020-09-15 16:29:23 -0300

    Codei a regra de colocar somente discos menores em cima do outro

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-let torres = document.querySelectorAll(".torres")
+let selectorMode = true
+let selectedTorre = undefined
+let selectDisc = 0
+let currentDisc = 0
 
-torres.forEach(item => item.addEventListener("click", movimento))
+document.querySelectorAll(".torres").forEach(item => item.addEventListener('click',moveDisco))
 
-function movimento (elemento) {
-    console.log(elemento.currentTarget)
-    console.log()
+function moveDisco(e) {
+    
+    let discInTorre = e.currentTarget.childElementCount
+
+    if (selectorMode) {
+        if (discInTorre) {
+            selectedTorre = e.currentTarget
+            selectDisc = e.currentTarget.lastElementChild.clientWidth
+            selectorMode = false
+            console.log(selectDisc)
+            console.log(selectedTorre)
+        } 
+    } else (insertDisco(e.currentTarget))
+
+    function insertDisco(e) {
+        if (discInTorre) {
+            currentDisc = e.lastElementChild.clientWidth
+            console.log(currentDisc)
+            if (selectDisc < currentDisc) {
+                e.appendChild(selectedTorre.lastElementChild)
+                selectorMode = true
+            } else {
+                alert("Você não pode colocar um disco maior em cima de um menor")
+                selectorMode = true
+            }
+        } else {
+            console.log(currentDisc)
+            e.appendChild(selectedTorre.lastElementChild)
+            selectorMode = true
+        }
+    }
+
 }
+
+
+
+    
+
